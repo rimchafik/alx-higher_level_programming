41cfe298267f0d1ca1074bf95af99c2c7122cf4c
rimchafik <rimchafik10@gmail.com>
2024-02-06 16:24:10 +0000

    Done

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+d_file function container"""
+
+
+def read_file(filename=""):
+    """""reads a text file(UTF8) and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as file:
+        files = file.read()
+        print(files, end="")
